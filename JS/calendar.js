4b1d20770be49b7227af1ed9c3097df8de590171
seoungju0828 <ministar2006@gmail.com>
2023-11-10 02:29:07 +0900

    GoBack 완성 : 캘린더 완성, 날씨 API 완성, 반응형 완성

--- JS/calendar.js
+++ JS/calendar.js
@@ -84,7 +84,6 @@ balloonDiv.classList.add("balloon");
 document.body.appendChild(balloonDiv);
 
 const setCalendar2 = (year, month) => {
-    // ...
 
     // 7의 배수인 날짜를 클릭할 때 말풍선을 보여주는 함수
     function showBalloon(event) {
@@ -96,7 +95,7 @@ const setCalendar2 = (year, month) => {
             const dateRect = clickedDate.getBoundingClientRect();
             balloonDiv.style.top = dateRect.bottom + "px";
             balloonDiv.style.left = dateRect.left + "px";
-            balloonDiv.textContent = `${dateValue}일: 7의 배수 날짜입니다!`;
+            balloonDiv.textContent = /*`${dateValue}일:*/ `LUCKY DAY!`;
             balloonDiv.style.display = "block";
         } else {
             // 7의 배수가 아닌 날짜를 클릭하면 말풍선 숨김
@@ -114,7 +113,7 @@ const setCalendar2 = (year, month) => {
 const balloonStyles = `
     .balloon {
         position: absolute;
-        background-color: #FF69B4; /* 분홍색 배경 */
+        background-color: green;
         color: white;
         padding: 10px;
         border-radius: 15px; /* 더 둥근 테두리 */
@@ -125,22 +124,77 @@ const balloonStyles = `
     }
 `;
 
-function showBalloon(event) {
-    const clickedDate = event.target;
-    const dateValue = parseInt(clickedDate.innerText);
+// function showBalloon(event) {
+//     const clickedDate = event.target;
+//     const dateValue = parseInt(clickedDate.innerText);
 
-    if (dateValue % 7 === 0) {
-        // 말풍선을 클릭한 날짜 위치 아래에 표시
-        const dateRect = clickedDate.getBoundingClientRect();
-        balloonDiv.style.top = dateRect.bottom + "px";
-        balloonDiv.style.left = dateRect.left + "px";
-        balloonDiv.innerHTML = `${dateValue}일: 7의 배수 날짜입니다! <i class="bi bi-heart-fill" style="font-size: 1.5rem;"></i>`;
-        balloonDiv.style.display = "block";
-    } else {
-        // 7의 배수가 아닌 날짜를 클릭하면 말풍선 숨김
-        balloonDiv.style.display = "none";
-    }
+//     if (dateValue % 7 === 0) {
+//         // 말풍선을 클릭한 날짜 위치 아래에 표시
+//         const dateRect = clickedDate.getBoundingClientRect();
+//         balloonDiv.style.top = dateRect.bottom + "px";
+//         balloonDiv.style.left = dateRect.left + "px";
+//         balloonDiv.innerHTML = `${dateValue}일: 7의 배수 날짜입니다! <i class="bi bi-heart-fill" style="font-size: 1.5rem;"></i>`;
+//         balloonDiv.style.display = "block";
+//     } else {
+//         // 7의 배수가 아닌 날짜를 클릭하면 말풍선 숨김
+//         balloonDiv.style.display = "none";
+//     }
+// }
+
+// function showBalloon(event) {
+//     const clickedDate = event.target;
+//     const dateValue = parseInt(clickedDate.innerText);
+//     const clickedMonth = month;  // 여기서 현재 월을 가져오고 있었는데, 수정이 필요해 보입니다.
+
+//     // 클릭한 날짜의 월 가져오기
+//     const clickedDateObj = new Date(year, month - 1, dateValue);
+//     const clickedDateMonth = clickedDateObj.getMonth() + 1;
+
+//     if (dateValue === 14 && clickedDateMonth === 2) {
+//         // 선택한 날짜가 2월 14일인지 확인
+//         const dateRect = clickedDate.getBoundingClientRect();
+//         balloonDiv.style.top = dateRect.bottom + "px";
+//         balloonDiv.style.left = dateRect.left + "px";
+//         balloonDiv.innerHTML = `해피 발렌타인 데이! <i class="bi bi-heart-fill" style="font-size: 1.5rem;"></i>`;
+//         balloonDiv.style.display = "block";
+//     } else if (dateValue % 7 === 0) {
+//         // 선택한 날짜가 7의 배수인지 확인
+//         const dateRect = clickedDate.getBoundingClientRect();
+//         balloonDiv.style.top = dateRect.bottom + "px";
+//         balloonDiv.style.left = dateRect.left + "px";
+//         balloonDiv.innerHTML = `${dateValue}일: 7의 배수 날짜입니다! <i class="bi bi-heart-fill" style="font-size: 1.5rem;"></i>`;
+//         balloonDiv.style.display = "block";
+//     } else {
+//         // 다른 날짜에는 말풍선을 숨깁니다.
+//         balloonDiv.style.display = "none";
+//     }
+// }
+
+// function showBalloon(event) {
+//     const clickedDate = event.target;
+//     const dateValue = parseInt(clickedDate.innerText);
+//     const clickedMonth = month;
+
+//     const clickedDateObj = new Date(year, month - 1, dateValue);
+//     const clickedDateMonth = clickedDateObj.getMonth() + 1;
+
+//     if (clickedMonth === 4 && dateValue === 4) {
+//         showCustomBalloon(clickedDate, "lucky!");
+//     } else if (dateValue % 7 === 0) {
+//         showCustomBalloon(clickedDate, `${dateValue}일: 럭키데이!`);
+//     } else {
+//         balloonDiv.style.display = "none";
+//     }
+// }
+
+function showCustomBalloon(clickedDate, message) {
+    const dateRect = clickedDate.getBoundingClientRect();
+    balloonDiv.style.top = dateRect.bottom + "px";
+    balloonDiv.style.left = dateRect.left + "px";
+    balloonDiv.innerHTML = `${message} <i class="bi bi-heart-fill" style="font-size: 1.5rem;"></i>`;
+    balloonDiv.style.display = "block";
 }
+
 
 const styleSheet = document.createElement("style");
 styleSheet.type = "text/css";
--- JS/weather.js
+++ JS/weather.js
@@ -3,12 +3,12 @@ const getJSON = function(url, callback) {
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function() {
-      const status = xhr.status;
-      if(status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
+        const status = xhr.status;
+        if(status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
     };
     xhr.send();
 };
@@ -16,52 +16,44 @@ const getJSON = function(url, callback) {
 getJSON('http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=1eb1d18602c0e2dde562cdc2005a4495&units=metric',
 
 function(err, data) {
-    if(err !== null) {
-      alert('예상치 못한 오류 발생.' + err);
-    } else {
-    //   document.write(`현재
-    //     온도는 ${data.main.temp}°
-    //     풍속은 ${data.wind.speed}m/s
-    //     습도는 ${data.main.humidity}%
-    //     입니다.
 
-    //     오늘의
-    //     최고기온은 ${data.main.temp_max}°
-    //     최저기온은 ${data.main.temp_min}°
-    //     입니다.`);
-    // }
-    alert(`현재
-        온도는 ${data.main.temp}°
-        풍속은 ${data.wind.speed}m/s
-        습도는 ${data.main.humidity}%
-        입니다.
+    var ment = "오늘 아닐리 X";
+    var temp = `온도 : ${data.main.temp}°`;
+    var wind = `풍속 : ${data.wind.speed}m/s`;
+    var humidity = `습도 : ${data.main.humidity}%`;
+    var temp_max = `최고 : ${data.main.temp_max}°`;
+    var temp_min = `최저 : ${data.main.temp_min}°`;
+    
+    // // HTML 요소의 내용을 설정합니다.
+    document.getElementById("ment").textContent = ment;
+    document.getElementById("temp").textContent = temp;
+    document.getElementById("wind").textContent = wind;
+    document.getElementById("humidity").textContent = humidity;
+    document.getElementById("temp_max").textContent = temp_max;
+    document.getElementById("temp_min").textContent = temp_min;
 
-        오늘의
-        최고기온은 ${data.main.temp_max}°
-        최저기온은 ${data.main.temp_min}°
-        입니다.`);
-    }
 });
 
 function getWeatherData(city) {
     const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=kr`;
   
     fetch(weatherURL)
-      .then(response => response.json())
-      .then(data => {
+        .then(response => response.json())
+        .then(data => {
         // 날씨 정보를 화면에 표시
-        const temperatureElement = document.querySelector('.temperature');
-        const placeElement = document.querySelector('.place');
-        const descriptionElement = document.querySelector('.description');
-  
-        temperatureElement.textContent = `${data.main.temp}°C`;
-        placeElement.textContent = data.name;
-        descriptionElement.textContent = data.weather[0].description;
-    })
-      .catch(error => {
-        console.error('날씨 정보를 가져오는 중 오류가 발생했습니다:', error);
-    });
+            const temperatureElement = document.querySelector('.temperature');
+            const placeElement = document.querySelector('.place');
+            const descriptionElement = document.querySelector('.description');
+            
+            temperatureElement.textContent = `${data.main.temp}°C`;
+            placeElement.textContent = data.name;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('날씨 정보를 가져오는 중 오류가 발생했습니다:', error);
+        }
+    );
 }
   
 // 서울의 날씨 정보를 가져옵니다. 다른 도시로 변경하려면 도시 이름을 수정하세요.
-getWeatherData('Seoul');
+getWeatherData('seoul');
